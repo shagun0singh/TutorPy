50d67994f68840ced309a49792cfab5f980d9268
shagunsinghs <shagun.singh2024@nst.rishihood.edu.in>
2025-12-02 22:21:54 +0530

    hello

---
+++ .anaconda/navigator/scripts/notebook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /opt/anaconda3/bin/activate /opt/anaconda3
+open /opt/anaconda3/bin/jupyter_mac.command >/Users/pali/.anaconda/navigator/scripts/notebook-out-1.txt 2>/Users/pali/.anaconda/navigator/scripts/notebook-err-1.txt
